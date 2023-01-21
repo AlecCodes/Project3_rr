6dbcfb8412c4dda388c2a2a89637adeddf4676ca
Alec Hannaford <alec322@gmail.com>
2023-01-21 08:26:29 -0800

    css styling and card containers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
 import {Outlet} from 'react-router-dom'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Outlet/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+function Footer(props){
+    return(
+    <footer className="footer">
+        <h1>footer footer fooeter</h1>
+    </footer>
+    )
+}
+
+export default Footer
