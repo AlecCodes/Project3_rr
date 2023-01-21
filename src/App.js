4741aa8d0b060326d43f658e1f71e07ec1e7d2c1
Alec Hannaford <alec322@gmail.com>
2023-01-21 11:57:37 -0800

    controlled form can filter results by keystroke after first character

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import Header from './Components/Header'
 import {Outlet} from 'react-router-dom'
 import Footer from './Components/Footer'
 import {useState} from 'react'
+import data from './dummydata'
 
 function App() {
   const [formState, setFormState] = useState('')
   function changeHandler(event){
     setFormState(event.target.value)
+    
+    const filtered = [...data].filter((element)=>{
+      if(formState.toUpperCase() === element.name.substring(0,formState.length).toUpperCase()){
+        return element
+      }
+    })
+
+    console.log(filtered)
   }
 
   return (
