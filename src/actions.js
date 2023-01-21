0ee7fceecf4bfd394b3b08a6426147e140199453
Jcemrick <jcemrick@gmail.com>
2023-01-21 12:18:55 -0800

    actions.js made with action routes

---
+++ src/actions.js
@@ -0,0 +1,50 @@
+import { redirect } from 'react-router-dom';
+
+const URL = 'https://backend-restaurant-review.onrender.com/';
+
+export const createAction = async ({request}) => {
+    const formData = await request.formData()
+    const newRestaurant = {
+        name: formData.get("name"),
+        image: formData.get("image"),
+        cuisine: formData.get("cuisine"),
+        address: formData.get("address")
+    }
+    await fetch(URL + "/restaurant", {
+        method: "post",
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(newRestaurant)
+    })
+    return redirect('/')
+}
+
+export const  updateAction = async ({request, params}) => {
+    const formData = await request.formData();
+    const newRestaurant = {
+        name: formData.get("name"),
+        image: formData.get("image"),
+        cuisine: formData.get("cuisine"),
+        address: formData.get("address")
+    }
+
+    await fetch(URL + `/restaurant/${params.id}`, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newRestaurant)
+    })
+    return redirect('/')
+}
+
+export const deleteAction = async ({params}) => {
+    await fetch(URL = `/restaurant/${params.id}`, {
+        method: 'delete',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    return redirect('/')
+}
