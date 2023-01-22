ae874fc4b0cb9b1399c0b7e5ea937ee2465696cf
Alec Hannaford <alec322@gmail.com>
2023-01-22 10:55:53 -0800

    hooked up loader to app.js to display data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './Components/Header'
-import {Outlet} from 'react-router-dom'
+import {Outlet, useLoaderData} from 'react-router-dom'
 import Footer from './Components/Footer'
 import Rating from './Components/Rating'
 import {useState} from 'react'
-import data from './dummydata'
+//import data from './dummydata'
 
 function App() {
+  const data = useLoaderData()
   const [dataState, setDataState] = useState(data)
   const [formState, setFormState] = useState('')
   function changeHandler(event){
