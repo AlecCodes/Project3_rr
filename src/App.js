1db16c2bfaa2ce6dfeddf9c842de8189c1ff98f7
Alec Hannaford <alec322@gmail.com>
2023-01-22 17:39:36 -0800

    cleaned up kludge from app js

--- src/App.js
+++ src/App.js
@@ -7,28 +7,11 @@ import {useState} from 'react'
 //import data from './dummydata'
 
 function App() {
-  const data = useLoaderData()
-  const [dataState, setDataState] = useState(data)
-  const [formState, setFormState] = useState('')
-  function changeHandler(event){
-    setFormState(event.target.value)
-    
-    const filtered = [...data].filter((element)=>{
-      if(formState.toUpperCase() === element.name.substring(0,formState.length).toUpperCase()){
-        return element
-      }
-    })
-    // console.log('VVVVV - Change handler executed - VVVVVVVV')
-    // console.log(filtered)
-    setDataState(filtered)
-
-  }
 
   return (
     <div className="App">
-      <Header value={formState} onChange={changeHandler}/>
-      <Outlet context={dataState}/>
-      {/* <Rating /> */}
+      <Header/>
+      <Outlet/>
       <Footer/>
     </div>
   );
